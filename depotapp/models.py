da2e5599fa9f12aa25c538b2e8278c0b32002efa
yanghongda <yanghongda2006@126.com>
2016-08-26 09:43:15 +0800

    创建app完成

--- depotapp/models.py
+++ depotapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url =  models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+
 
 # Create your models here.
